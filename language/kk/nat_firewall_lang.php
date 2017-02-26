9803b8e0c7e1a2529ff0a89c9ffdcefb64e55f5e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:03:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1-ге 1 NAT ереже қосу';
+$lang['nat_firewall_app_description'] = '1-ге 1 NAT Firewall қолданба қоғамдық IP-адрес арқылы брандмауэр жүйелерге қол жеткізуін беретін жеке IP мекен-жайы бойынша қоғамдық IP мекенжайын салыстырады.';
+$lang['nat_firewall_app_name'] = '1-ге 1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-ге 1 NAT кем дегенде бір сыртқы теңшелген желі интерфейсін талап етеді.';
+$lang['nat_firewall_port_or_port_range'] = 'Port немесе порт ауқымын';
+$lang['nat_firewall_private_ip'] = 'Жеке IP';
+$lang['nat_firewall_public_ip'] = 'Мемлекеттік IP';
+$lang['nat_firewall_rules'] = '1-ге 1 NAT ережелері';
+$lang['nat_firewall_tooltip'] = 'Бұл мүмкіндік, интернет провайдеріңізден (ISP) қосымша қоғамдық IP мекенжайларын талап етеді.';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
