9c223705d83c2aa926143c72e61212fa1f3ec000
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:04:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1-ടു-1 NAT ലേക്ക് നിയമം ചേർക്കുക';
+$lang['nat_firewall_app_description'] = '1-ടു-1 NAT ലേക്ക് ഫയർവാൾ അപ്ലിക്കേഷൻ ഒരു പൊതു IP വിലാസം വഴി ഫയർവാൾ പിന്നിൽ സിസ്റ്റങ്ങൾ ആക്സസ് അനുവദിക്കുന്ന ഒരു സ്വകാര്യ IP വിലാസം ഒരു പൊതു IP വിലാസം മാപ്പുചെയ്യുന്നു.';
+$lang['nat_firewall_app_name'] = '1-ടു-1 NAT ലേക്ക് ഫയർവാൾ';
+$lang['nat_firewall_lan_ip'] = 'ലാൻ ഐപി';
+$lang['nat_firewall_no_external_nic'] = '1-ടു-1 NAT ലേക്ക് കുറഞ്ഞത് ഒരു ബാഹ്യമായി ക്രമീകരിച്ച നെറ്റ്വർക്ക് ഇന്റർഫെയിസ് ആവശ്യമാണ്.';
+$lang['nat_firewall_port_or_port_range'] = 'പോര്ട് അല്ലെങ്കിൽ പോർട്ട് പരിധി';
+$lang['nat_firewall_private_ip'] = 'സ്വകാര്യ ഐ.പി.';
+$lang['nat_firewall_public_ip'] = 'പൊതു ഐപി';
+$lang['nat_firewall_rules'] = '1-ടു-1 NAT ലേക്ക് നിയമങ്ങൾ';
+$lang['nat_firewall_tooltip'] = 'ഈ സവിശേഷത നിങ്ങളുടെ ഇന്റർനെറ്റ് സേവന ദാതാവിനെ (ISP) മുതൽ അധിക പൊതു ഐപി വിലാസങ്ങൾ ആവശ്യമാണ്.';
+$lang['nat_firewall_wan_ip'] = 'വാൻ ഐപി';
