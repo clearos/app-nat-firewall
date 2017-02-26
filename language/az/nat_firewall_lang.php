342051d022d4226a5ce49763f7929f5ab556ff1a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:58:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1-to-1 NAT Qayda əlavə et';
+$lang['nat_firewall_app_description'] = '1-to-1 NAT Firewall app ictimai IP ünvanı vasitəsilə firewall arxasında sistemləri erişim imkan xüsusi IP ünvanı ictimai IP ünvan xəritələr.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT ən azı bir xarici konfiqurasiya şəbəkə interfeysi tələb edir.';
+$lang['nat_firewall_port_or_port_range'] = 'Port və ya Port Range';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT qaydaları';
+$lang['nat_firewall_tooltip'] = 'Bu xüsusiyyət Internet Service Provider (ISP) əlavə ictimai IP ünvanlar tələb edir.';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
