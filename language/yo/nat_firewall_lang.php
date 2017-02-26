c8d782e0003a0863c6d692c8537bf403b1de7b55
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:09:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Fi 1-si-1 Nat Ofin';
+$lang['nat_firewall_app_description'] = '1-si-1 Nat Firewall app maapu kan àkọsílẹ IP adirẹsi si ikọkọ IP adirẹsi gbigba wiwọle si awọn ọna šiše sile awọn ogiriina nipasẹ kan àkọsílẹ IP adirẹsi.';
+$lang['nat_firewall_app_name'] = '1-to-1 Nat Firewall';
+$lang['nat_firewall_lan_ip'] = 'lan IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 Nat nbeere ni o kere ọkan externally tunto nẹtiwọki ni wiwo.';
+$lang['nat_firewall_port_or_port_range'] = 'Port tabi Port Range';
+$lang['nat_firewall_private_ip'] = 'ikọkọ IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = '1-to-1 Nat Ofin';
+$lang['nat_firewall_tooltip'] = 'Ẹya ara ẹrọ yi nilo afikun àkọsílẹ IP adirẹsi lati rẹ Olupese Iṣẹ Ayelujara (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
