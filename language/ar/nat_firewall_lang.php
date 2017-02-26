30d1ae4447bdab378eacd8e9e16ccc28708eee49
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:57:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'إضافة 1 إلى 1 NAT القاعدة';
+$lang['nat_firewall_app_description'] = 'وNAT جدار حماية التطبيق 1 إلى 1 خرائط عنوان IP العام إلى عنوان IP الخاص مما يتيح الوصول إلى أنظمة وراء جدار الحماية عن طريق عنوان IP العام.';
+$lang['nat_firewall_app_name'] = '1 إلى 1 NAT جدار حماية';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1 إلى 1 NAT يتطلب واحد على الأقل واجهة الشبكة تكوين خارجيا.';
+$lang['nat_firewall_port_or_port_range'] = 'ميناء أو منفذ المدى';
+$lang['nat_firewall_private_ip'] = 'IP الخاص';
+$lang['nat_firewall_public_ip'] = 'IP العام';
+$lang['nat_firewall_rules'] = '1 إلى 1 قواعد NAT';
+$lang['nat_firewall_tooltip'] = 'تتطلب هذه الميزة عناوين إضافية الملكية الفكرية العامة من موفر خدمة الإنترنت (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
