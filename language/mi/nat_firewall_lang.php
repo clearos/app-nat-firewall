17b3c57ecd32965ad807b7837c7c8726ba011dbd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:04:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Tāpiri Ture NAT 1-ki-1';
+$lang['nat_firewall_app_description'] = 'mahere te NAT Pātūahi taupānga 1-ki-1 te wāhitau IP tūmatanui ki te wāhitau IP tūmataiti tuku uru ki ngā pūnaha i muri i te pātūahi mā te wāhitau IP tūmatanui.';
+$lang['nat_firewall_app_name'] = '1-ki-1 NAT Pātūahi';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = 'titau NAT 1-ki-1 i te iti rawa kotahi atanga whatunga waho whirihora.';
+$lang['nat_firewall_port_or_port_range'] = 'Port Port Range ranei';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'IP tūmatanui';
+$lang['nat_firewall_rules'] = '1-ki-1 NAT Ture';
+$lang['nat_firewall_tooltip'] = 'Me whiwhi tēnei āhuatanga atu wāhitau IP tūmatanui i tou Kaiwhakarato Ratonga Ipurangi (ISP).';
+$lang['nat_firewall_wan_ip'] = 'wan IP';
