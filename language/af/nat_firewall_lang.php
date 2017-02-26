c9387f3b3ce56374affd344aca36447286d62827
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:57:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Voeg 1-tot-1 NAT Reël';
+$lang['nat_firewall_app_description'] = 'Die 1-tot-1 NAT Firewall app kaarte n openbare IP-adres na &#39;n private IP-adres verlening van toegang tot stelsels agter die firewall via &#39;n openbare IP-adres.';
+$lang['nat_firewall_app_name'] = '1-tot-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-tot-1 NAT vereis ten minste een ekstern opgestel netwerk koppelvlak.';
+$lang['nat_firewall_port_or_port_range'] = 'Port of Port Range';
+$lang['nat_firewall_private_ip'] = 'private IP';
+$lang['nat_firewall_public_ip'] = 'openbare IP';
+$lang['nat_firewall_rules'] = '1-tot-1 NAT Reëls';
+$lang['nat_firewall_tooltip'] = 'Hierdie funksie vereis bykomende openbare IP-adresse van jou Internet Diens Verskaffer (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
