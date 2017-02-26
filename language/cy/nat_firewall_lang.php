0541ddfb5e457b86f9c246a73577718917b045af
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:09:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Ychwanegu 1-i-1 NAT Rheol';
+$lang['nat_firewall_app_description'] = 'Mae&#39;r NAT Firewall app 1-i-1 fapiau cyfeiriad IP cyhoeddus i gyfeiriad IP preifat caniatáu mynediad i systemau y tu ôl i&#39;r wal dân drwy gyfeiriad IP cyhoeddus.';
+$lang['nat_firewall_app_name'] = '1-i-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-i-1 NAT gofyn am o leiaf un rhyngwyneb rhwydwaith ffurfweddu yn allanol.';
+$lang['nat_firewall_port_or_port_range'] = 'Port neu Port Range';
+$lang['nat_firewall_private_ip'] = 'IP preifat';
+$lang['nat_firewall_public_ip'] = 'IP cyhoeddus';
+$lang['nat_firewall_rules'] = '1-i-1 Rheolau NAT';
+$lang['nat_firewall_tooltip'] = 'Mae&#39;r nodwedd hon yn ei gwneud yn ofynnol cyfeiriadau IP cyhoeddus ychwanegol gan eich Darparwr Gwasanaeth Rhyngrwyd (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
