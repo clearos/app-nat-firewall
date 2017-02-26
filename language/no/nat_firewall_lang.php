39390cfe29836dff63b951b321ded7cb610beb22
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:05:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Legg en-til-en NAT regel';
+$lang['nat_firewall_app_description'] = 'Den en-til-en NAT brannmur app maps en offentlig IP-adresse til en privat IP-adresse slik at tilgang til systemer bak brannmuren via en offentlig IP-adresse.';
+$lang['nat_firewall_app_name'] = 'En-til-en NAT-brannmur';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = 'En-til-en NAT krever minst ett eksternt konfigurert nettverksgrensesnitt.';
+$lang['nat_firewall_port_or_port_range'] = 'Port eller Port Range';
+$lang['nat_firewall_private_ip'] = 'private IP';
+$lang['nat_firewall_public_ip'] = 'offentlig IP';
+$lang['nat_firewall_rules'] = '1-til-1 NAT regler';
+$lang['nat_firewall_tooltip'] = 'Denne funksjonen krever ekstra offentlige IP-adresser fra Internet Service Provider (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
