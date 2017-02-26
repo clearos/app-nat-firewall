ad1ad842b795934ab6bdf9f527efb49a90980f9b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:08:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1 க்கு 1 NAT விதி சேர்க்கவும்';
+$lang['nat_firewall_app_description'] = '1 க்கு 1 இந்த NAT ஃபயர்வால் பயன்பாட்டை ஒரு பொது ஐபி முகவரி வழியாக தீச்சுவரால் அமைப்புகள் அனுமதிக்கலாம் ஒரு தனியார் IP முகவரி ஒரு பொது ஐபி முகவரி வரைபடங்கள்.';
+$lang['nat_firewall_app_name'] = '1 க்கு 1 இந்த NAT ஃபயர்வால்';
+$lang['nat_firewall_lan_ip'] = 'லேன் ஐபி';
+$lang['nat_firewall_no_external_nic'] = '1 க்கு 1 இந்த NAT குறைந்தது ஒரு வெளிப்புறமாக கட்டமைக்கப்பட்ட பிணைய முகப்பை தேவைப்படுகிறது.';
+$lang['nat_firewall_port_or_port_range'] = 'போர்ட் அல்லது போர்ட் வீச்சை';
+$lang['nat_firewall_private_ip'] = 'தனியார் ஐபி';
+$lang['nat_firewall_public_ip'] = 'பொது ஐபி';
+$lang['nat_firewall_rules'] = '1 க்கு 1 இந்த NAT விதிகள்';
+$lang['nat_firewall_tooltip'] = 'இந்த வசதியை உங்கள் இணைய சேவை வழங்குநர் (ISP) சில பொது ஐபி முகவரிகள் தேவைப்படுகிறது.';
+$lang['nat_firewall_wan_ip'] = 'WAN ஐபி';
