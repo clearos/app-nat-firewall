22eb7530c7990ee5bbf2591711826faed8c882f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:08:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1 నుంచి 1 NAT రూల్ జోడించండి';
+$lang['nat_firewall_app_description'] = '1 నుంచి 1 NAT ఫైర్వాల్ అనువర్తనం పబ్లిక్ IP చిరునామా ద్వారా ఫైర్వాల్ వెనుక వ్యవస్థలు యాక్సెస్ అనుమతిస్తుంది ఒక ప్రైవేట్ IP చిరునామాకు ఒక పబ్లిక్ IP చిరునామా పటాలు.';
+$lang['nat_firewall_app_name'] = '1 నుంచి 1 NAT ఫైర్వాల్';
+$lang['nat_firewall_lan_ip'] = 'LAN ఐపీ';
+$lang['nat_firewall_no_external_nic'] = '1 నుంచి 1 NAT కనీసం ఒక బాహ్యంగా ఆకృతీకరణ నెట్వర్క్ ఇంటర్ఫేస్ అవసరం.';
+$lang['nat_firewall_port_or_port_range'] = 'పోర్ట్ లేదా పోర్ట్ రేంజ్';
+$lang['nat_firewall_private_ip'] = 'ప్రైవేట్ IP';
+$lang['nat_firewall_public_ip'] = 'పబ్లిక్ IP';
+$lang['nat_firewall_rules'] = '1 నుంచి 1 NAT రూల్స్';
+$lang['nat_firewall_tooltip'] = 'ఈ ఫీచర్ మీ ఇంటర్నెట్ సర్వీస్ ప్రొవైడర్ (ISP) నుండి అదనపు పబ్లిక్ IP చిరునామాలను అవసరం.';
+$lang['nat_firewall_wan_ip'] = 'పాలిపోయిన ఐపీ';
