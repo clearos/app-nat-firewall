23c3308be29b5271eb1922ad09e640245f2aee97
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:05:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Żid Regola 1-to-1 NAT';
+$lang['nat_firewall_app_description'] = 'Il-app NAT Firewall 1-to-1 mapep indirizz IP pubbliku għal indirizz IP privat li jippermettu aċċess għal sistemi wara l-firewall permezz ta &#39;indirizz IP pubbliku.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN PI';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT teħtieġ mill-inqas wieħed interkonnessjoni ma &#39;netwerk konfigurati esternament.';
+$lang['nat_firewall_port_or_port_range'] = 'Port jew Medda port';
+$lang['nat_firewall_private_ip'] = 'privat PI';
+$lang['nat_firewall_public_ip'] = 'PI pubblika';
+$lang['nat_firewall_rules'] = '1-to-1 Regoli TAN';
+$lang['nat_firewall_tooltip'] = 'Din il-karatteristika jeħtieġ indirizzi addizzjonali IP pubbliku mill tiegħek Internet Service Provider (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN PI';
