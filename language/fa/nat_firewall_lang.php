5d376dd40348e092498f8d85be4a9837c3074488
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:05:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'اضافه کردن 1 به 1 NAT قانون';
+$lang['nat_firewall_app_description'] = 'برنامه 1 به 1 از NAT فایروال نقشه یک آدرس IP عمومی به یک آدرس IP خصوصی اجازه می دهد دسترسی به سیستم در پشت دیوار آتش از طریق یک آدرس IP عمومی.';
+$lang['nat_firewall_app_name'] = '1 به 1 از NAT فایروال';
+$lang['nat_firewall_lan_ip'] = 'شبکه آی پی';
+$lang['nat_firewall_no_external_nic'] = '1 به 1 NAT نیاز به حداقل یک رابط شبکه خارجی پیکربندی شده است.';
+$lang['nat_firewall_port_or_port_range'] = 'پورت و یا پورت محدوده';
+$lang['nat_firewall_private_ip'] = 'IP خصوصی';
+$lang['nat_firewall_public_ip'] = 'IP عمومی';
+$lang['nat_firewall_rules'] = '1 به 1 از قوانین NAT';
+$lang['nat_firewall_tooltip'] = 'این قابلیت نیاز به آدرس های IP عمومی اضافی را از خود ارائه دهنده خدمات اینترنت (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
