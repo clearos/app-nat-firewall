b18c7663594a4a343c91f0d41c610d01971eba2b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:03:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1 : 1 NAT 규칙 추가';
+$lang['nat_firewall_app_description'] = '1 대 1 NAT 방화벽 애플리케이션은 공용 IP 어드레스를 통해 방화벽 시스템에 대한 액세스를 가능하게하는 사설 IP 주소를 공용 IP 주소를 맵핑한다.';
+$lang['nat_firewall_app_name'] = '1 : 1 NAT 방화벽';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1 : 1 NAT는 적어도 하나의 외부로 구성된 네트워크 인터페이스를 필요로한다.';
+$lang['nat_firewall_port_or_port_range'] = '포트 또는 포트 범위';
+$lang['nat_firewall_private_ip'] = '개인 IP';
+$lang['nat_firewall_public_ip'] = '공공 IP';
+$lang['nat_firewall_rules'] = '1 : 1 NAT 규칙';
+$lang['nat_firewall_tooltip'] = '이 기능은 인터넷 서비스 제공 업체 (ISP)에서 추가 공용 IP 주소가 필요합니다.';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
