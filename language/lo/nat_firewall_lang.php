232efde240ceba225b60820e86d74791b7399428
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:03:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'ເພີ່ມກົດ 1 ກັບ 1 NAT';
+$lang['nat_firewall_app_description'] = '1-to-1 NAT Firewall app ແຜນທີ່ທີ່ຢູ່ IP ສາທາລະນະທີ່ຢູ່ IP ເອກະຊົນໃຫ້ສາມາດເຂົ້າເຖິງລະບົບການທາງຫລັງຂອງໂຄງການໂດຍຜ່ານທີ່ຢູ່ IP ສາທາລະນະ.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT ຮຽກຮ້ອງໃຫ້ມີຢ່າງຫນ້ອຍຫນຶ່ງໃນການໂຕ້ຕອບເຄືອຂ່າຍການກໍາຫນົດຄ່າພາຍນອກ.';
+$lang['nat_firewall_port_or_port_range'] = 'Port ຫຼື Port Range';
+$lang['nat_firewall_private_ip'] = 'IP ເອກະຊົນ';
+$lang['nat_firewall_public_ip'] = 'IP ສາທາລະນະ';
+$lang['nat_firewall_rules'] = '1-to-1 ກົດລະບຽບ NAT';
+$lang['nat_firewall_tooltip'] = 'ຄຸນນະສົມບັດນີ້ຮຽກຮ້ອງໃຫ້ມີທີ່ຢູ່ IP ສາທາລະນະເພີ່ມເຕີມຈາກຂອງທ່ານໃຫ້ບໍລິການບໍລິການອິນເຕີເນັດ (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
