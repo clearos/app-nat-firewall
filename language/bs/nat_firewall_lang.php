2a2ae7cf5a277f294294609d182337e9ca6a57b6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:58:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Dodaj 1-na-1 NAT Rule';
+$lang['nat_firewall_app_description'] = 'NAT Firewall aplikaciju 1-na-1 na zemljovidu javnu IP adresu na privatne IP adrese koji omogućava pristup sisteme iza zida preko javnih IP adresa.';
+$lang['nat_firewall_app_name'] = '1-na-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-na-1 NAT zahtijeva barem jedan izvana konfiguriran mrežno sučelje.';
+$lang['nat_firewall_port_or_port_range'] = 'Port ili Port Range';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Javna IP';
+$lang['nat_firewall_rules'] = '1-na-1 NAT pravila';
+$lang['nat_firewall_tooltip'] = 'Ova funkcija zahtijeva dodatne javne IP adrese iz vašeg Internet usluga (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
