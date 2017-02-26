c4790b5bdc359ea50fc48247c20b79ca69dc26fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:58:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Gehitu 1-to-1 NAT Araua';
+$lang['nat_firewall_app_description'] = '1 eta 1 NAT Firewall Aplikazioa IP publiko bat maps IP pribatuak helbide bat firewall atzean sistemak sarbidea IP publiko baten bidez, aukera ematen du.';
+$lang['nat_firewall_app_name'] = '1 eta 1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1 eta 1 NAT bat behintzat kanpotik konfiguratutako sare interfaze eskatzen.';
+$lang['nat_firewall_port_or_port_range'] = 'Port edo Port Range';
+$lang['nat_firewall_private_ip'] = 'IP pribatuak';
+$lang['nat_firewall_public_ip'] = 'IP Publikoa';
+$lang['nat_firewall_rules'] = '1 eta 1 NAT arauak';
+$lang['nat_firewall_tooltip'] = 'Ezaugarri honek zure hornitzailea (ISP) tik IP helbide publikoak gehigarria eskatzen.';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
