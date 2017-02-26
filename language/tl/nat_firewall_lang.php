548d377abecf9f369abde0a2093a11eb74e6609d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:08:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Magdagdag ng 1-sa-1 NAT Rule';
+$lang['nat_firewall_app_description'] = 'Ang 1-sa-1 NAT Firewall app mga mapa ng isang pampublikong IP address sa isang pribadong IP address na nagpapahintulot ng access sa mga sistema sa likod ng firewall sa pamamagitan ng isang pampublikong IP address.';
+$lang['nat_firewall_app_name'] = '1-sa-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'lAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-sa-1 NAT ay nangangailangan ng hindi bababa sa isang panlabas isinaayos network interface.';
+$lang['nat_firewall_port_or_port_range'] = 'Port o Port Range';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = '1-sa-1 NAT Panuntunan';
+$lang['nat_firewall_tooltip'] = 'Ang tampok na ito ay nangangailangan ng karagdagang pampublikong IP address mula sa iyong Internet Service Provider (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
