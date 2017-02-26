7247b6598b28a9175472e954c49829b441758973
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:05:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Tambah 1-to-1 NAT Peraturan';
+$lang['nat_firewall_app_description'] = 'Aplikasi 1-to-1 NAT Firewall memetakan alamat IP awam ke alamat IP persendirian yang membolehkan akses kepada sistem di belakang firewall melalui alamat IP awam.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT memerlukan sekurang-kurangnya satu muka rangkaian luaran dikonfigurasikan.';
+$lang['nat_firewall_port_or_port_range'] = 'Port atau Pelabuhan Range';
+$lang['nat_firewall_private_ip'] = 'IP persendirian';
+$lang['nat_firewall_public_ip'] = 'IP awam';
+$lang['nat_firewall_rules'] = '1-to-1 Peraturan NAT';
+$lang['nat_firewall_tooltip'] = 'Ciri ini memerlukan alamat IP awam tambahan daripada Pembekal Khidmat Internet (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
