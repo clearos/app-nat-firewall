95d3d6de74e5eeecd1ed12b807d8f05dbcdcc4d5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:02:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Tambahkan Aturan 1-to-1 NAT';
+$lang['nat_firewall_app_description'] = '1-to-1 NAT Firewall aplikasi memetakan sebuah alamat IP publik ke alamat IP pribadi yang memungkinkan akses ke sistem di belakang firewall melalui alamat IP publik.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT membutuhkan setidaknya satu antarmuka jaringan dikonfigurasi secara eksternal.';
+$lang['nat_firewall_port_or_port_range'] = 'Pelabuhan atau pelabuhan Rentang';
+$lang['nat_firewall_private_ip'] = 'swasta IP';
+$lang['nat_firewall_public_ip'] = 'IP publik';
+$lang['nat_firewall_rules'] = '1-to-1 Aturan NAT';
+$lang['nat_firewall_tooltip'] = 'Fitur ini memerlukan tambahan alamat IP publik dari Internet Service Provider (ISP).';
+$lang['nat_firewall_wan_ip'] = 'IP WAN';
