4bd8b84d5b6bf7df14139d45759d614480e368e3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:06:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Se adaugă 1-la-1 NAT Rule';
+$lang['nat_firewall_app_description'] = 'NAT Firewall App 1-la-1 maps o adresă IP publică la o adresă IP privată care să permită accesul la sistemele din spatele firewall-ului printr-o adresă IP publică.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'IP LAN';
+$lang['nat_firewall_no_external_nic'] = '1-la-1 NAT are nevoie de cel puțin o interfață de rețea configurată în exterior.';
+$lang['nat_firewall_port_or_port_range'] = 'Port sau de Port';
+$lang['nat_firewall_private_ip'] = 'IP privat';
+$lang['nat_firewall_public_ip'] = 'IP publică';
+$lang['nat_firewall_rules'] = '1-la-1 Reguli NAT';
+$lang['nat_firewall_tooltip'] = 'Această funcție necesită adrese IP publice suplimentare de la furnizorul de servicii Internet (ISP).';
+$lang['nat_firewall_wan_ip'] = 'IP WAN';
