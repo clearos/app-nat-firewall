e4b27fc0f8a48d085c1adebae0a2e53ab2f0175b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:08:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Иловаи 1-ба-1 NAT Ќоидаи';
+$lang['nat_firewall_app_description'] = 'Дар барнома 1-ба-1 NAT Сипар харитаҳои суроғаи IP ҷамъиятӣ ба суроғаи IP хусусӣ имкон дастрасӣ ба системаҳои пушти Сипар тавассути суроғаи IP ҷамъиятӣ.';
+$lang['nat_firewall_app_name'] = '1-ба-1 NAT Сипар';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-ба-1 NAT талаб ақал як интерфейси шабакаро берунӣ танзим карда.';
+$lang['nat_firewall_port_or_port_range'] = 'Порт-ё Порт-диапазон';
+$lang['nat_firewall_private_ip'] = 'IP хусусӣ';
+$lang['nat_firewall_public_ip'] = 'IP ҷамъиятии';
+$lang['nat_firewall_rules'] = '1-ба-1 Қоидаҳои NAT';
+$lang['nat_firewall_tooltip'] = 'Ин хусусият талаб адресҳои IP ҷамъиятӣ иловагї аз шумо дастраскунандаи хадамоти Интернет (ДХИ).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
