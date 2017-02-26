1508f0190040675db4cbbc6e8b85f50c3ccadb34
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:58:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Добавя се 1-към-1 NAT правило';
+$lang['nat_firewall_app_description'] = 'NAT Firewall Приложението 1-към-1 карта на публична IP адрес на частен IP адрес, което позволява достъп до системи, които стоят зад защитната стена чрез IP адрес публично.';
+$lang['nat_firewall_app_name'] = '1-към-1 Firewall NAT';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-към-1 NAT изисква поне едно външно конфигуриран мрежов интерфейс.';
+$lang['nat_firewall_port_or_port_range'] = 'Порт или Port Range';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = '1-към-1 NAT правила';
+$lang['nat_firewall_tooltip'] = 'Тази функция изисква допълнителни публични IP адреси от вашия доставчик на интернет услуги (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
