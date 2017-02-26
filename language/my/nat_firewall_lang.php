616bf7b26a9a31dc48a7fa48754d4565bfeba459
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:59:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1-To-1 NAT နည်းဥပဒေ Add';
+$lang['nat_firewall_app_description'] = 'အဆိုပါ 1-To-1 NAT Firewall ကို app ကိုအများပြည်သူ IP address ကိုတဆင့် firewall ကနောက်ကွယ်မှစနစ်များကိုအသုံးပြုခွင့်ကိုခွင့်ပြုပုဂ္ဂလိက IP address ကိုမှအများပြည်သူ IP address ကိုမြေပုံ။';
+$lang['nat_firewall_app_name'] = '1-To-1 NAT Firewall ကို';
+$lang['nat_firewall_lan_ip'] = 'LAN IP ကို';
+$lang['nat_firewall_no_external_nic'] = '1-To-1 NAT အနည်းဆုံးပြင်ပမှ configured network interface ကိုလိုအပ်သည်။';
+$lang['nat_firewall_port_or_port_range'] = 'ဆိပ်ကမ်းသို့မဟုတ်ဆိပ်ကမ်း Range';
+$lang['nat_firewall_private_ip'] = 'ပုဂ္ဂလိကအိုင်ပီ';
+$lang['nat_firewall_public_ip'] = 'ပြည်သူ့အိုင်ပီ';
+$lang['nat_firewall_rules'] = '1-To-1 NAT စည်းကမ်းများ';
+$lang['nat_firewall_tooltip'] = 'ဒီ feature သင်၏ Internet Service Provider (ISP က) ကနေနောက်ထပ်အများပြည်သူ IP လိပ်စာများလိုအပ်သည်။';
+$lang['nat_firewall_wan_ip'] = 'WAN IP ကို';
