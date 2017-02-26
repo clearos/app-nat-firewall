ee8209e1d92f7d2d36f196f06d4e1ad8810b5927
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:03:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'დამატება 1-to-1 NAT Rule';
+$lang['nat_firewall_app_description'] = '1-to-1 NAT Firewall app რუკები საჯარო IP მისამართი კერძო IP მისამართი საშუალებას ხელმისაწვდომობის სისტემები უკან firewall მეშვეობით საჯარო IP მისამართი.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT მოითხოვს მინიმუმ ერთი გარე ქსელის ინტერფეისი.';
+$lang['nat_firewall_port_or_port_range'] = 'პორტი ან პორტის Range';
+$lang['nat_firewall_private_ip'] = 'პირადი IP';
+$lang['nat_firewall_public_ip'] = 'საჯარო IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT წესები';
+$lang['nat_firewall_tooltip'] = 'ეს ფუნქცია მოითხოვს დამატებითი საჯარო IP მისამართები თქვენი ინტერნეტ პროვაიდერ (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
