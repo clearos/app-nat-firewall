2db0006d6bb2c7d167ef407020d8fcffe2be854e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:04:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Add 1-to-1 NAT Fitsipika';
+$lang['nat_firewall_app_description'] = 'Ny 1-to-1 NAT Firewall rindrambaiko Manapa-panjakana adiresy IP ho iray tsy miankina adiresy IP mamela ny fidirana amin&#39;ny rafitra ao ambadiky ny Rindrin&#39;afo ampahibemaso amin&#39;ny alalan&#39;ny adiresy IP.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT iray, fara fahakeliny dia mitaky voaendrika Network interface tsara ety ivelany.';
+$lang['nat_firewall_port_or_port_range'] = 'Port na Port Range';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT Fitsipika';
+$lang['nat_firewall_tooltip'] = 'Endri-javatra io dia mitaky ny adiresy IP-bahoaka fanampiny avy amin&#39;ny Internet Service Provider (ISP).';
+$lang['nat_firewall_wan_ip'] = 'IP WAN';
