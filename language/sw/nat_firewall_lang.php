33aca929279780657f8591621cb3082090167dd4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:07:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Kuongeza 1-to-1 NAT Rule';
+$lang['nat_firewall_app_description'] = 'programu 1-to-1 NAT Firewall ramani IP ya umma kwa anwani binafsi IP kuruhusu upatikanaji wa mifumo ya nyuma firewall kupitia anwani IP ya umma.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT inahitaji angalau moja nje kimeundwa interface mtandao.';
+$lang['nat_firewall_port_or_port_range'] = 'Port au Port Mbalimbali';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT Rules';
+$lang['nat_firewall_tooltip'] = 'Kipengele hiki inahitaji anwani za ziada za IP ya umma kutoka Internet Service Provider yako (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
