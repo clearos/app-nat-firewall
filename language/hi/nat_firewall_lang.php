67b7782cb106f2e19b3f7c5546503beb6ce93a45
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:01:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1 से 1 नेट नियम जोड़ें';
+$lang['nat_firewall_app_description'] = '1 से 1 नेट फ़ायरवॉल एप्लिकेशन को एक सार्वजनिक आईपी पते के माध्यम से फ़ायरवॉल के पीछे प्रणालियों के लिए उपयोग की अनुमति एक निजी आईपी पते के एक सार्वजनिक आईपी पता नक्शे।';
+$lang['nat_firewall_app_name'] = '1 से 1 नेट फ़ायरवॉल';
+$lang['nat_firewall_lan_ip'] = 'लैन आईपी';
+$lang['nat_firewall_no_external_nic'] = '1 से 1 नेट कम से कम एक बाह्य रूप से कॉन्फ़िगर नेटवर्क इंटरफेस की आवश्यकता है।';
+$lang['nat_firewall_port_or_port_range'] = 'पोर्ट या पोर्ट रेंज';
+$lang['nat_firewall_private_ip'] = 'निजी आईपी';
+$lang['nat_firewall_public_ip'] = 'सार्वजनिक आईपी';
+$lang['nat_firewall_rules'] = '1 से 1 नेट नियम';
+$lang['nat_firewall_tooltip'] = 'यह सुविधा आपके इंटरनेट सेवा प्रदाता (आईएसपी) से अतिरिक्त सार्वजनिक आईपी पतों की आवश्यकता है।';
+$lang['nat_firewall_wan_ip'] = 'वान आईपी';
