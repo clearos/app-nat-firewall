dcf9723ecd11306a5c70fc41143d6ccb497aedac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:02:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Tinye 1 na-1 Nat Ọma ahụ';
+$lang['nat_firewall_app_description'] = 'The 1-aka-1 Nat Firewall ngwa map a ọha adreesị IP ka a onwe adreesị IP ikwe ohere usoro n&#39;azụ firewall via a ọha adreesị IP.';
+$lang['nat_firewall_app_name'] = '1-aka-1 Nat Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-aka-1 Nat na-achọ dịkarịa ala otu externally ahazi netwọk interface.';
+$lang['nat_firewall_port_or_port_range'] = 'Port ma ọ bụ Port iche';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = '1-aka-1 Nat Rules';
+$lang['nat_firewall_tooltip'] = 'A mma na-achọ ndị ọzọ ọha adreesị IP gị Internet Service Provider (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
