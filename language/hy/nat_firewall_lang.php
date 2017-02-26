d5e4e2bf0c661066896a0eda143b6556912b359f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:57:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Ավելացնել 1-to-1 NAT Կանոն';
+$lang['nat_firewall_app_description'] = '1-ից 1 NAT Firewall ծրագիրը քարտեզները հանրային IP հասցե մասնավոր IP հասցեն թույլ տալով մուտք դեպի համակարգերի ետեւում firewall միջոցով հանրային IP հասցեն.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-ից 1-NAT պահանջում է առնվազն մեկ արտաքուստ կոնֆիգուրացիաները ցանցային ինտերֆեյսի.';
+$lang['nat_firewall_port_or_port_range'] = 'Պորտ կամ Պորտ Range';
+$lang['nat_firewall_private_ip'] = 'անձնական IP';
+$lang['nat_firewall_public_ip'] = 'Հանրային IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT Օրենքները';
+$lang['nat_firewall_tooltip'] = 'Այս հատկությունը պահանջում է լրացուցիչ հանրային IP հասցեներ Ձեր Ինտերնետ - ծառայությունների մատակարար (ISP):';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
