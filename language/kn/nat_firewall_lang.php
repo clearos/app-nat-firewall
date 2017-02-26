3cf250d8c5228eec551275bba7074ec43b74824b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:03:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1 ಯಾ 1 NAT ನಿಯಮವನ್ನು ಸೇರಿಸಿ';
+$lang['nat_firewall_app_description'] = '1 ಯಾ 1 NAT ಫೈರ್ವಾಲ್ ಅಪ್ಲಿಕೇಶನ್ ಸಾರ್ವಜನಿಕ IP ವಿಳಾಸವನ್ನು ಮೂಲಕ ಫೈರ್ವಾಲ್ ಹಿಂದೆ ಸಿಸ್ಟಮ್ಗಳಿಗೆ ಪ್ರವೇಶವನ್ನು ಅವಕಾಶ ಖಾಸಗಿ IP ವಿಳಾಸವನ್ನು ಸಾರ್ವಜನಿಕ IP ವಿಳಾಸ ನಕ್ಷೆಗಳು.';
+$lang['nat_firewall_app_name'] = '1 ಯಾ 1 NAT ಫೈರ್ವಾಲ್';
+$lang['nat_firewall_lan_ip'] = 'ಲ್ಯಾನ್ ಐಪಿ';
+$lang['nat_firewall_no_external_nic'] = '1 ಯಾ 1 NAT ಕನಿಷ್ಠ ಒಂದು ಬಾಹ್ಯವಾಗಿ ಕಾನ್ಫಿಗರ್ ನೆಟ್ವರ್ಕ್ ಇಂಟರ್ಫೇಸ್ ಅಗತ್ಯವಿದೆ.';
+$lang['nat_firewall_port_or_port_range'] = 'ಪೋರ್ಟ್ ಅಥವಾ ಪೋರ್ಟ್ ರೇಂಜ್';
+$lang['nat_firewall_private_ip'] = 'ಖಾಸಗಿ IP';
+$lang['nat_firewall_public_ip'] = 'ಸಾರ್ವಜನಿಕ IP';
+$lang['nat_firewall_rules'] = '1 ಯಾ 1 NAT ನಿಯಮಗಳು';
+$lang['nat_firewall_tooltip'] = 'ಈ ವೈಶಿಷ್ಟ್ಯವು ನಿಮ್ಮ ಇಂಟರ್ನೆಟ್ ಸೇವೆ ಒದಗಿಸುವವರಿಗೆ (ISP) ಹೆಚ್ಚುವರಿ ಸಾರ್ವಜನಿಕ IP ವಿಳಾಸಗಳನ್ನು ಅಗತ್ಯವಿದೆ.';
+$lang['nat_firewall_wan_ip'] = 'ವಾನ್ ಐಪಿ';
