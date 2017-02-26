073f02dfea61417a20ce51839091263ed70161ab
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:07:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Tillsätt 1-till-1 NAT Rule';
+$lang['nat_firewall_app_description'] = '1-till-1 NAT-brandvägg app avbildar en allmän IP-adress till en privat IP-adress som ger tillgång till system bakom brandväggen via en publik IP-adress.';
+$lang['nat_firewall_app_name'] = '1-till-1 NAT-brandvägg';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-till-1 NAT kräver åtminstone ett externt konfigurerade nätverksgränssnitt.';
+$lang['nat_firewall_port_or_port_range'] = 'Porten eller portintervallet';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = '1-till-1 NAT regler';
+$lang['nat_firewall_tooltip'] = 'Den här funktionen kräver ytterligare publika IP-adresser från din Internet Service Provider (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
