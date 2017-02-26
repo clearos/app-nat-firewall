8f8c3f1cbc1fa5607dacd4d6065846185a488a2a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:05:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1-देखि-1 NAT नियम थप्न';
+$lang['nat_firewall_app_description'] = '1-देखि-1 NAT फायरवल अनुप्रयोग एक निजी IP ठेगाना सार्वजनिक IP ठेगाना मार्फत फायरवल पछि प्रणाली पहुँच अनुमति गर्न सार्वजनिक IP ठेगाना म्याप।';
+$lang['nat_firewall_app_name'] = '1-देखि-1 NAT फायरवल';
+$lang['nat_firewall_lan_ip'] = 'लैन आईपी';
+$lang['nat_firewall_no_external_nic'] = '1-देखि-1 NAT कम्तीमा पनि एक बाह्य कन्फिगर सञ्जाल इन्टरफेस आवश्यक छ।';
+$lang['nat_firewall_port_or_port_range'] = 'पोर्ट वा पोर्ट सीमा';
+$lang['nat_firewall_private_ip'] = 'निजी आईपी';
+$lang['nat_firewall_public_ip'] = 'सार्वजनिक IP';
+$lang['nat_firewall_rules'] = '1-देखि-1 NAT नियम';
+$lang['nat_firewall_tooltip'] = 'यो सुविधा आफ्नो इन्टरनेट सेवा प्रदायक (ISP) देखि अतिरिक्त सार्वजनिक IP ठेगाना आवश्यक छ।';
+$lang['nat_firewall_wan_ip'] = 'Wan आईपी';
