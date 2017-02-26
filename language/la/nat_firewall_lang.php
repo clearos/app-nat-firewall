321f997a6d57e3f444f2f8e7e1d43def33226f7b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:03:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Add I-ad-I Reg NAT';
+$lang['nat_firewall_app_description'] = 'De I-ad-I NAT Firewall App maps publicum a privato ad IP ad IP oratio aditum systemata post Firewall via publica IP oratio.';
+$lang['nat_firewall_app_name'] = 'I I NAT Firewall ut-';
+$lang['nat_firewall_lan_ip'] = 'IP LAN';
+$lang['nat_firewall_no_external_nic'] = 'I-ad-I NAT extra figuram requirit saltem unum network interface.';
+$lang['nat_firewall_port_or_port_range'] = 'Range et portus, Port';
+$lang['nat_firewall_private_ip'] = 'Secretum IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = 'I-ut-I NAT Rules';
+$lang['nat_firewall_tooltip'] = 'Hoc pluma publicum requirit addito ex Penitus Muneris Suggero IP (ISP).';
+$lang['nat_firewall_wan_ip'] = 'IP PUTRIS';
