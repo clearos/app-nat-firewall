046ffb846756ccd399d91ab4b637a66d89734f3b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:07:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Add Rule 1-in-1 NAT';
+$lang['nat_firewall_app_description'] = 'The NAT Brannmur app 1-in-1 maps cinwaan IP dadweynaha cinwaan IP gaar ah jidaynayey helitaanka nidaamka dambeeya brannmur via cinwaan IP dadweynaha.';
+$lang['nat_firewall_app_name'] = '1-in-1 NAT Brannmur';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-in-1 NAT waxay u baahan tahay ugu yaraan hal dibadda qaybiyay interface network.';
+$lang['nat_firewall_port_or_port_range'] = 'Port ama Port Range';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = '1-in-1 NAT Rules';
+$lang['nat_firewall_tooltip'] = 'Habkaani wuxuu u baahan yahay cinwaanada IP dadweynaha dheeraad ah ka Provider Service Internet (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
