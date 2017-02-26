a6267a499dbbe97c249c990cba0b9cb1e21588d6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:00:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Aldonu 1-al-1 NAT Regulo';
+$lang['nat_firewall_app_description'] = 'La 1-al-1 NAT Firewall app mapoj publika IP al privata IP permesante aliro al sistemoj malantaŭ la fajromuro tra publika IP adreso.';
+$lang['nat_firewall_app_name'] = '1-al-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'Lan IP';
+$lang['nat_firewall_no_external_nic'] = '1-al-1 NAT postulas almenaŭ ekstere agordita reta interfaco.';
+$lang['nat_firewall_port_or_port_range'] = 'Haveno aŭ haveno Gamo';
+$lang['nat_firewall_private_ip'] = 'privata IP';
+$lang['nat_firewall_public_ip'] = 'publika IP';
+$lang['nat_firewall_rules'] = '1-al-1 NAT Reguloj';
+$lang['nat_firewall_tooltip'] = 'Tiu funkcio postulas aldonan publika IP de via Interreta Servo Provizanto (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
