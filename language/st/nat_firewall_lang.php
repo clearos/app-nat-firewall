edec891b5a5e61c8e900a23b969620c2f7368a4c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:07:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Add 1-to-1 Nat Rule';
+$lang['nat_firewall_app_description'] = 'Ka sediriswa 1-to-1 Nat Pula Tsa Lehlohonolo Maps e aterese IP sechaba ho e aterese ya poraefete IP lumella mekhoa ea ho fumana litsamaiso ka morao fayawolo ea ka lipalangoang tsa aterese IP phatlalatsa.';
+$lang['nat_firewall_app_name'] = '1-to-1 Nat Pula Tsa Lehlohonolo';
+$lang['nat_firewall_lan_ip'] = 'Lan IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 Nat hloka bonyane e kwa di kentswe segokanyimmediamentsi sa sebolokigolo netweke.';
+$lang['nat_firewall_port_or_port_range'] = 'Port kapa Port Range';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = '1-to-1 Nat Rules';
+$lang['nat_firewall_tooltip'] = 'Tsobotsi ena e hloka liaterese tsa tlatsetso IP setjhaba tsoang Internet Service Mohlokomeli ea hao (ISP).';
+$lang['nat_firewall_wan_ip'] = 'Wan IP';
