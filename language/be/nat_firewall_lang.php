8ee8f49055123e81b78538f3d6b3eaeb1641b6ad
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:58:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Дадаць правіла 1-к-1 NAT';
+$lang['nat_firewall_app_description'] = 'Брандмаўэр NAT Дадатак 1-к-1 адлюстроўвае публічны IP-адрас на прыватны IP-адрас, які дазваляе доступ да сістэм за брандмаўарам праз IP-адрас.';
+$lang['nat_firewall_app_name'] = '1-к-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'IP LAN';
+$lang['nat_firewall_no_external_nic'] = '1-к-1 NAT патрабуе, па меншай меры, адзін вонкава наладжаны сеткавы інтэрфейс.';
+$lang['nat_firewall_port_or_port_range'] = 'Порт або дыяпазон партоў';
+$lang['nat_firewall_private_ip'] = 'прыватны IP';
+$lang['nat_firewall_public_ip'] = 'адкрыты IP';
+$lang['nat_firewall_rules'] = '1-к-1 Правілы NAT';
+$lang['nat_firewall_tooltip'] = 'Гэта функцыя патрабуе дадатковых агульнадаступных IP-адрасоў ад вашага пастаўшчыка паслуг Інтэрнэту (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
