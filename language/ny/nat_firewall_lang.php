058be2a0bcfcb1027ca788455465267c2a113933
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:59:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Kuwonjezera 1-to-1 NAT Rule';
+$lang['nat_firewall_app_description'] = 'The app 1-to-1 NAT makhoma oteteza mapu adilesi IP poyera adilesi payekha IP kulola kupeza kachitidwe kumbuyo makhoma oteteza kudzera adilesi IP pagulu.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT makhoma oteteza';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT amafuna chimodzi kunja kukhazikitsidwa mawonekedwe Intaneti.';
+$lang['nat_firewall_port_or_port_range'] = 'Port kapena Port osiyanasiyana';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT Malamulo';
+$lang['nat_firewall_tooltip'] = 'Mbali imeneyi amafuna maadiresi zina IP pamaso pa Internet Service Provider wanu (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
