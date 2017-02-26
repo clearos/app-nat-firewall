eccd46b8bcdd67dd122541070649e850c0094607
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:59:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '加1对1的NAT规则';
+$lang['nat_firewall_app_description'] = '在1对1 NAT防火墙应用的公共IP地址映射到一个专用IP地址，允许通过一个公网IP地址访问防火墙后面的系统。';
+$lang['nat_firewall_app_name'] = '1对1的NAT防火墙';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1对1的NAT需要至少一个外部配置的网络接口。';
+$lang['nat_firewall_port_or_port_range'] = '端口或端口范围';
+$lang['nat_firewall_private_ip'] = '私有IP';
+$lang['nat_firewall_public_ip'] = '公网IP';
+$lang['nat_firewall_rules'] = '1对1的NAT规则';
+$lang['nat_firewall_tooltip'] = '此功能需要从Internet服务提供商（ISP），额外的公共IP地址。';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
