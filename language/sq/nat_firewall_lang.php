ce26a855c6819007aca8d9ac284ffc96bce2b3cc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:57:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Shto Rregullën 1-to-1 NAT';
+$lang['nat_firewall_app_description'] = 'Aplikacioni 1-me-1 NAT Firewall hartat një adresë IP publike në një adresë IP private duke lejuar qasje në sistemet prapa firewall nëpërmjet një adresë IP publike.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT kërkon paktën një ndërfaqe të konfiguruar jashtmi rrjetit.';
+$lang['nat_firewall_port_or_port_range'] = 'Port ose Port Gama';
+$lang['nat_firewall_private_ip'] = 'IP private';
+$lang['nat_firewall_public_ip'] = 'IP publik';
+$lang['nat_firewall_rules'] = '1-to-1 Rregullat e NAT';
+$lang['nat_firewall_tooltip'] = 'Ky funksion kërkon adresat shtesë IP publike nga Internet Service Provider tuaj (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
