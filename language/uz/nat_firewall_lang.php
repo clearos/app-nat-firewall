d0c9827adb0f6fcb3ad7ef08d8093771c2081d42
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:09:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1-to-1 NAT qoida qo&#39;shish';
+$lang['nat_firewall_app_description'] = '1-to-1 NAT Firewall ilova jamoat IP manzil orqali xavfsizlik devorining ortida tizimlari kirish imkonini beruvchi maxsus IP manzilga jamoat IP manzilini, xaritalar.';
+$lang['nat_firewall_app_name'] = '1-to-1 Gen xavfsizlik devorining';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT kamida bir tashqi tuzilgan tarmoq interfeysi talab qiladi.';
+$lang['nat_firewall_port_or_port_range'] = 'Port yoki port oralig&#39;i';
+$lang['nat_firewall_private_ip'] = 'xususiy IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT qoidalari';
+$lang['nat_firewall_tooltip'] = 'Bu xususiyat Internet xizmat ko&#39;rsatuvchi provayderingiz (ISP) qo&#39;shimcha ijtimoiy IP manzillar talab qiladi.';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
