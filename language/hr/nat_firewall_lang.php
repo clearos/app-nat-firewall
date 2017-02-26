51a701368cce1f913ffa89ec15677305a9bf4dd8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:59:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Dodati 1-na-1 NAT pravila';
+$lang['nat_firewall_app_description'] = '1-na-1 app NAT vatrozid maps javnu IP adresu na privatnu IP adresu dozvoljava pristup sustavima iza vatrozida putem javne IP adrese.';
+$lang['nat_firewall_app_name'] = '1-na-1 NAT vatrozid';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-na-1 NAT zahtijeva barem jedan izvana mrežnu sučelje.';
+$lang['nat_firewall_port_or_port_range'] = 'Port ili luke raspona';
+$lang['nat_firewall_private_ip'] = 'Privatna IP';
+$lang['nat_firewall_public_ip'] = 'Javna IP';
+$lang['nat_firewall_rules'] = '1-na-1 NAT pravila';
+$lang['nat_firewall_tooltip'] = 'Ova značajka zahtijeva dodatne javne IP adrese iz vašeg davatelja internetskih usluga (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
