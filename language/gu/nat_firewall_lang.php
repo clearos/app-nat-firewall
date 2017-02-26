7655b29001b52e6e072df7d7d63524b3be9bd650
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:01:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1 થી 1 NAT નિયમ ઉમેરો';
+$lang['nat_firewall_app_description'] = '1 થી 1 NAT ફાયરવોલ એપ્લિકેશન જાહેર IP સરનામું મારફતે ફાયરવોલ પાછળ સિસ્ટમો માટે પ્રવેશને પરવાનગી આપે છે એક ખાનગી IP સરનામા માટે જાહેર IP સરનામું દર્શાવે છે.';
+$lang['nat_firewall_app_name'] = '1 થી 1 NAT ફાયરવોલ';
+$lang['nat_firewall_lan_ip'] = 'લેન IP';
+$lang['nat_firewall_no_external_nic'] = '1 થી 1 NAT ઓછામાં ઓછા એક બાહ્ય રૂપરેખાંકિત નેટવર્ક ઇન્ટરફેસને જરૂરી છે.';
+$lang['nat_firewall_port_or_port_range'] = 'પોર્ટ અથવા પોર્ટ વિસ્તારને';
+$lang['nat_firewall_private_ip'] = 'ખાનગી IP';
+$lang['nat_firewall_public_ip'] = 'જાહેર IP';
+$lang['nat_firewall_rules'] = '1 થી 1 NAT નિયમો';
+$lang['nat_firewall_tooltip'] = 'આ સુવિધા તમારા ઇન્ટરનેટ સેવા પ્રદાતા (ISP) માંથી વધારાની જાહેર IP સરનામાઓ જરૂરી છે.';
+$lang['nat_firewall_wan_ip'] = 'નિસ્તેજ આઇપી';
