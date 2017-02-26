05e85af692958802fbdc7e81181489199bf80762
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:09:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Engeza 1-to-1 NAT Rule';
+$lang['nat_firewall_app_description'] = 'Insiza 1-to-1 NAT firewall Ibeka ikheli le-IP yomphakathi ikheli le-IP yangasese uvumela ukufinyelela izinhlelo ngemuva firewall nge ikheli le IP yomphakathi.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT kudinga okungenani eyodwa ngaphandle amisiwe inethiwekhi esibonakalayo.';
+$lang['nat_firewall_port_or_port_range'] = 'Port noma Port Range';
+$lang['nat_firewall_private_ip'] = 'IP Private';
+$lang['nat_firewall_public_ip'] = 'IP Public';
+$lang['nat_firewall_rules'] = '1-to-1 NAT Rules';
+$lang['nat_firewall_tooltip'] = 'Lesi sici sidinga eyengeziwe amakheli IP zomphakathi kusuka yakho Kumhlinzeki Wesevisi Ye-Inthanethi (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
