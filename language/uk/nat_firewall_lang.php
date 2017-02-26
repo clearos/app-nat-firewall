748224abb9f97fba4b6e6f98df626733658adef2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:08:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Додати правило 1-к-1 NAT';
+$lang['nat_firewall_app_description'] = 'Брандмауер NAT Додаток 1-к-1 відображає публічний IP-адресу на приватний IP-адреса, що дозволяє доступ до систем за брандмауером через IP-адресу.';
+$lang['nat_firewall_app_name'] = '1-к-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'IP LAN';
+$lang['nat_firewall_no_external_nic'] = '1-к-1 NAT вимагає, щонайменше, один зовні налаштований мережевий інтерфейс.';
+$lang['nat_firewall_port_or_port_range'] = 'Порт або діапазон портів';
+$lang['nat_firewall_private_ip'] = 'приватний IP';
+$lang['nat_firewall_public_ip'] = 'відкритий IP';
+$lang['nat_firewall_rules'] = '1-к-1 Правила NAT';
+$lang['nat_firewall_tooltip'] = 'Ця функція вимагає додаткових загальнодоступних IP-адрес від вашого постачальника послуг Інтернету (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
