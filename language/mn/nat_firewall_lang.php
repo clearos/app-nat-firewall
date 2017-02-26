c8ad310e07f218344d57766fa34ffa86e3e9e94c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:04:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1-тулд-1 NAT дүрэм нэмж бичээрэй';
+$lang['nat_firewall_app_description'] = '1-тулд-1 NAT Firewall програм нь олон нийтийн IP хаяг ашиглан галт ханын цаана системд хандах боломжтой бол хувийн IP хаяг руу олон нийтийн IP хаяг олгоно.';
+$lang['nat_firewall_app_name'] = '1-тулд-1 NAT Галт ханын тухай';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-тулд-1 NAT наад зах нь нэг гадаад тохируулсан сүлжээний интерфэйсийг шаарддаг.';
+$lang['nat_firewall_port_or_port_range'] = 'Порт юм уу эсвэл портын Range';
+$lang['nat_firewall_private_ip'] = 'Хувийн IP';
+$lang['nat_firewall_public_ip'] = 'Олон нийтийн IP';
+$lang['nat_firewall_rules'] = '1-тулд-1 NAT дүрэм';
+$lang['nat_firewall_tooltip'] = 'Энэ боломж нь таны интернетийн үйлчилгээ үзүүлэгч (ISP) нэмэлт нийтийн IP хаяг шаарддаг.';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
