1edae328173336d3d96f1d8a9f4c9d124fe31c0d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:08:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'เพิ่มกฎข้อที่ 1 ต่อ 1 NAT';
+$lang['nat_firewall_app_description'] = '1 ต่อ 1 NAT Firewall พลิเคชันแผนที่ที่อยู่ IP ของประชาชนที่จะใช้ที่อยู่ IP เอกชนที่ช่วยให้การเข้าถึงระบบหลังไฟร์วอลล์ผ่านทางที่อยู่ IP สาธารณะ';
+$lang['nat_firewall_app_name'] = '1 ต่อ 1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'IP ที่';
+$lang['nat_firewall_no_external_nic'] = '1 ต่อ 1 NAT ต้องมีอย่างน้อยหนึ่งในการกำหนดค่าเชื่อมต่อเครือข่ายภายนอก';
+$lang['nat_firewall_port_or_port_range'] = 'พอร์ตหรือช่วงพอร์ต';
+$lang['nat_firewall_private_ip'] = 'IP เอกชน';
+$lang['nat_firewall_public_ip'] = 'IP สาธารณะ';
+$lang['nat_firewall_rules'] = '1 ต่อ 1 กฎ NAT';
+$lang['nat_firewall_tooltip'] = 'คุณลักษณะนี้ต้องการที่อยู่ IP สาธารณะเพิ่มเติมจากผู้ให้บริการอินเทอร์เน็ต (ISP)';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
