038767527f529b13824ef4d0fd89532cd9b581bb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:01:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Adjunk hozzá 1 az 1-NAT szabály';
+$lang['nat_firewall_app_description'] = 'Az 1-to-1 NAT tűzfal alkalmazás térképek nyilvános IP-cím egy privát IP-címet, amely lehetővé teszi rendszerekhez való hozzáférés a tűzfal mögött egy nyilvános IP-címet.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT tűzfal';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT szerint legalább egy külsőleg beállított hálózati interfész.';
+$lang['nat_firewall_port_or_port_range'] = 'Port vagy Port Range';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'nyilvános IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT szabályok';
+$lang['nat_firewall_tooltip'] = 'Ez a funkció csak további publikus IP-címet az Internet Service Provider (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
