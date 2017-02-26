5a71caa6387f339870941d12302559487bef965f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:06:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Додати 1-на-1 НАТ правило';
+$lang['nat_firewall_app_description'] = 'НАТ Фиревалл цца 1-на-1 мапс јавну ИП адресу приватној ИП адреси омогућава приступ системима иза зида преко јавном ИП адресом.';
+$lang['nat_firewall_app_name'] = '1-на-1 НАТ Фиревалл';
+$lang['nat_firewall_lan_ip'] = 'ЛАН ИП';
+$lang['nat_firewall_no_external_nic'] = '1-на-1 НАС захтева бар једну споља подесио мрежни интерфејс.';
+$lang['nat_firewall_port_or_port_range'] = 'Порта или опсег портова';
+$lang['nat_firewall_private_ip'] = 'приватна ИП';
+$lang['nat_firewall_public_ip'] = 'јавна ИП';
+$lang['nat_firewall_rules'] = '1-на-1 НАТ Правила';
+$lang['nat_firewall_tooltip'] = 'Ова функција захтева додатну јавност ИП адресе из вашег Интернет провајдера (ИСП).';
+$lang['nat_firewall_wan_ip'] = 'ВАН ИП';
