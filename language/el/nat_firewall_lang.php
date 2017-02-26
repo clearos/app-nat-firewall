99fc9bffa09f0b341b235dead0c547a25150b227
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:01:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Προσθέστε 1-προς-1 NAT Κανόνας';
+$lang['nat_firewall_app_description'] = 'Το 1-προς-1 NAT Τείχος προστασίας εφαρμογών χαρτογραφεί μια δημόσια διεύθυνση IP σε μια ιδιωτική διεύθυνση IP που επιτρέπει την πρόσβαση σε συστήματα πίσω από το τείχος προστασίας μέσω μιας δημόσιας διεύθυνσης IP.';
+$lang['nat_firewall_app_name'] = '1-προς-1 NAT Τείχος προστασίας';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-προς-1 ΝΑΤ απαιτεί τουλάχιστον ένα εξωτερικά διαμορφωμένο διασύνδεση δικτύου.';
+$lang['nat_firewall_port_or_port_range'] = 'Λιμάνι ή Port Range';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'δημόσια IP';
+$lang['nat_firewall_rules'] = '1-προς-1 Κανόνες ΝΑΤ';
+$lang['nat_firewall_tooltip'] = 'Αυτή η λειτουργία απαιτεί πρόσθετες δημόσιες διευθύνσεις IP από την υπηρεσία παροχής Internet (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
