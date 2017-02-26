07f0d257f6ea33126fb6bd1a31d7d10c2fccd4fc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:02:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Bæta við 1-til-1 NAT reglu';
+$lang['nat_firewall_app_description'] = 'The 1-til-1 NAT Firewall app maps opinbera IP tölu til einka IP tölu leyfa aðgang að kerfum bak við eldvegg í gegnum opinber IP tölu.';
+$lang['nat_firewall_app_name'] = '1-til-1 NAT eldvegg';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-til-1 NAT þarf að minnsta kosti einn utan stillt net tengi.';
+$lang['nat_firewall_port_or_port_range'] = 'Port eða Port Range';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = '1-til-1 NAT Reglur';
+$lang['nat_firewall_tooltip'] = 'Þessi eiginleiki krefst frekari opinber IP-tölur frá þinn Internet Service Provider (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
