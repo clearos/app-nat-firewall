b214f51bd0ef52cdd3a4fb75f1d18b4326c749e0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:05:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Voeg 1-to-1 NAT Rule';
+$lang['nat_firewall_app_description'] = 'De 1-to-1 NAT Firewall app brengt een openbaar IP-adres aan een particulier IP-adres waardoor de toegang tot systemen achter de firewall via een openbaar IP-adres.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT vereist ten minste één extern geconfigureerd netwerkinterface.';
+$lang['nat_firewall_port_or_port_range'] = 'Port of Port Range';
+$lang['nat_firewall_private_ip'] = 'private IP';
+$lang['nat_firewall_public_ip'] = 'openbare IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT Rules';
+$lang['nat_firewall_tooltip'] = 'Deze functie vereist extra openbare IP-adressen van uw Internet Service Provider (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
