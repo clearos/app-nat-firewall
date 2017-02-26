bcb04d67c6e288a9c88f1d797edbb22c2ba877ee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:08:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1-to-1 NAT Kuralı Ekle';
+$lang['nat_firewall_app_description'] = '1-to-1 NAT Güvenlik Duvarı uygulaması bir genel IP adresi üzerinden güvenlik duvarı arkasında sistemlerine erişim sağlayan özel bir IP adresi için bir genel IP adresi eşler.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT en az bir harici olarak yapılandırılmış ağ arabirimi gerektirir.';
+$lang['nat_firewall_port_or_port_range'] = 'Liman veya Port Aralığı';
+$lang['nat_firewall_private_ip'] = 'özel IP';
+$lang['nat_firewall_public_ip'] = 'kamu IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT kuralları';
+$lang['nat_firewall_tooltip'] = 'Bu özellik, İnternet Servis Sağlayıcı (ISS) ek kamu IP adresleri gerektirir.';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
