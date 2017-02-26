4f107cb20570fe7ae62d9f4390a27ecf321a977a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:58:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1 টু 1 ন্যাট রুল যোগ';
+$lang['nat_firewall_app_description'] = '1 টু 1 ন্যাট ফায়ারওয়াল অ্যাপ্লিকেশন একটি সার্বজনীন IP ঠিকানা মাধ্যমে ফায়ারওয়ালের পিছনে সিস্টেম অ্যাক্সেস অনুমতি একটি ব্যক্তিগত IP ঠিকানার সাথে একটি পাবলিক আইপি ঠিকানা মানচিত্র.';
+$lang['nat_firewall_app_name'] = '1 টু 1 ন্যাট ফায়ারওয়াল';
+$lang['nat_firewall_lan_ip'] = 'ল্যান আইপি';
+$lang['nat_firewall_no_external_nic'] = '1 টু 1 ন্যাট অন্তত এক বাহ্যিকভাবে কনফিগার নেটওয়ার্ক ইন্টারফেস প্রয়োজন.';
+$lang['nat_firewall_port_or_port_range'] = 'পোর্ট বা বন্দর বিন্যাস';
+$lang['nat_firewall_private_ip'] = 'ব্যক্তিগত আইপি';
+$lang['nat_firewall_public_ip'] = 'জন আইপি';
+$lang['nat_firewall_rules'] = '1 টু 1 ন্যাট বিধি';
+$lang['nat_firewall_tooltip'] = 'এই বৈশিষ্ট্যটি আপনার ইন্টারনেট সেবা সরবরাহকারী (ISP) থেকে অতিরিক্ত পাবলিক আইপি অ্যাড্রেস দরকার.';
+$lang['nat_firewall_wan_ip'] = 'অস্পষ্ট আইপি';
