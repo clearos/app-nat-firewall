a18645f6d3019cbc130a3cdfdfa7fc9f65e69aaf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:03:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'បន្ថែមវិធាន 1 ទៅ 1 ត';
+$lang['nat_firewall_app_description'] = '1 ទៅ 1 ជញ្ជាំងភ្លើងកម្មវិធីផែនទីតអាសយដ្ឋាន IP ជាសាធារណៈទៅអាសយដ្ឋាន IP ឯកជនមួយដែលអនុញ្ញាតឱ្យចូលដំណើរការទៅប្រព័ន្ធពីក្រោយជញ្ជាំងភ្លើងតាមរយៈអាសយដ្ឋាន IP ជាសាធារណៈ។';
+$lang['nat_firewall_app_name'] = '1 ទៅ 1 តជញ្ជាំងភ្លើង';
+$lang['nat_firewall_lan_ip'] = 'IP ដែលបណ្តាញមូលដ្ឋាន';
+$lang['nat_firewall_no_external_nic'] = 'ត 1 ទៅ 1 ចំណុចប្រទាក់បណ្ដាញដែលបានកំណត់រចនាសម្ព័ន្ធតម្រូវឱ្យមានយ៉ាងហោចណាស់មួយខាងក្រៅ។';
+$lang['nat_firewall_port_or_port_range'] = 'ច្រកឬជួរច្រក';
+$lang['nat_firewall_private_ip'] = 'ឯកជន IP របស់';
+$lang['nat_firewall_public_ip'] = 'IP សាធារណៈ';
+$lang['nat_firewall_rules'] = '1 ទៅ 1 ក្បួនត';
+$lang['nat_firewall_tooltip'] = 'លក្ខណៈពិសេសនេះតម្រូវឱ្យមានអាសយដ្ឋាន IP ជាសាធារណៈបន្ថែមទៀតពីក្រុមហ៊ុនផ្ដល់សេវាអ៊ីនធឺណិត (ISP) ។';
+$lang['nat_firewall_wan_ip'] = 'IP ដែល WAN';
