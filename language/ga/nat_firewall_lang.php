f789eaef8c9fbb2e7fb26204b8d612166d4515fc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:02:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Cuir 1-le-1 NAT Riail';
+$lang['nat_firewall_app_description'] = 'léarscáileanna An 1-le-1 NAT Firewall app ar ainm IP poiblí le seoladh IP príobháideach a cheadaíonn rochtain ar chórais taobh thiar de na balla dóiteáin trí seoladh IP poiblí.';
+$lang['nat_firewall_app_name'] = '1-le-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = 'Éilíonn 1-le-1 TAN amháin ar a laghad comhéadan líonra cumraithe go seachtrach.';
+$lang['nat_firewall_port_or_port_range'] = 'Port nó Port Range';
+$lang['nat_firewall_private_ip'] = 'IP príobháideacha';
+$lang['nat_firewall_public_ip'] = 'IP poiblí';
+$lang['nat_firewall_rules'] = '1-le-1 Rialacha NAT';
+$lang['nat_firewall_tooltip'] = 'Éilíonn an gné seoltaí breise IP poiblí ó do Soláthraí Seirbhíse Idirlín (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
