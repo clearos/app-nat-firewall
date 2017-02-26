978d2e782330685ee6d4fc35bcaf1e0e215704d5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:59:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Přidejte 1-to-1 pravidlem NAT';
+$lang['nat_firewall_app_description'] = '1-to-1 aplikace NAT Firewall mapuje veřejnou IP adresu na privátní IP adresu umožňující přístup k systémům za firewallem přes veřejnou IP adresu.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-k-1 NAT vyžaduje alespoň jeden z vnějšku nakonfigurován síťové rozhraní.';
+$lang['nat_firewall_port_or_port_range'] = 'Port nebo rozsah portů';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'veřejná IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT Rules';
+$lang['nat_firewall_tooltip'] = 'Tato funkce vyžaduje dodatečné veřejné IP adresy od vašeho poskytovatele internetových služeb (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
