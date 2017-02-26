b6d23dc387df858486eac327bed3b06f3754094c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:09:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1 سے 1 NAT اصول شامل کریں';
+$lang['nat_firewall_app_description'] = '1 سے 1 NAT فصیل اے پی پی کے ایک عوامی IP ایڈریس کے ذریعے فائروال کے پیچھے نظام تک رسائی کی اجازت دی ایک نجی IP ایڈریس پر ایک عوامی IP ایڈریس نقشے.';
+$lang['nat_firewall_app_name'] = '1 سے 1 NAT فصیل';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1 سے 1 NAT کم از کم ایک بیرونی ترتیب دیا نیٹ ورک انٹرفیس کی ضرورت ہوتی ہے.';
+$lang['nat_firewall_port_or_port_range'] = 'پورٹ یا پورٹ رینج';
+$lang['nat_firewall_private_ip'] = 'نجی IP';
+$lang['nat_firewall_public_ip'] = 'عوامی IP';
+$lang['nat_firewall_rules'] = '1 سے 1 NAT کے قوانین';
+$lang['nat_firewall_tooltip'] = 'یہ خصوصیت آپ کے انٹرنیٹ سروس فراہم کرنے والے (ISP) سے اضافی عوامی IP ایڈریس کی ضرورت ہے.';
+$lang['nat_firewall_wan_ip'] = 'وان IP';
