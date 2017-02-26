2a693abbb67e411ccb8b21780c5810b6e3e9a51a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:00:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Lisa 1-to-1 NAT reegel';
+$lang['nat_firewall_app_description'] = '1-to-1 NAT tulemüüri rakendus kaardid avaliku IP aadressi era IP-aadress, mis võimaldab juurdepääsu süsteemid tulemüüri taga kaudu avaliku IP aadressi.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT tulemüüri';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT nõuab vähemalt ühte väliselt konfigureeritud võrguliidese.';
+$lang['nat_firewall_port_or_port_range'] = 'Port või Pordivahemik';
+$lang['nat_firewall_private_ip'] = 'Privaataadresside';
+$lang['nat_firewall_public_ip'] = 'avalik IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT reeglid';
+$lang['nat_firewall_tooltip'] = 'See funktsioon nõuab täiendavat avalikku IP aadresse oma internetiteenuse pakkuja (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
