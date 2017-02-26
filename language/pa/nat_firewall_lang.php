bcf966e30423f4b13e4ed236608f13a3f1183a3f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:05:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1-ਨੂੰ-1 NAT ਨਿਯਮ ਜੋੜੋ';
+$lang['nat_firewall_app_description'] = '1-ਨੂੰ-1 NAT ਫਾਇਰਵਾਲ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਇੱਕ ਪਬਲਿਕ IP ਪਤਾ ਦੁਆਰਾ ਫਾਇਰਵਾਲ ਦੇ ਪਿੱਛੇ ਸਿਸਟਮ ਤੱਕ ਪਹੁੰਚ, ਜਿਸ ਨਾਲ ਇੱਕ ਨਿੱਜੀ IP ਪਤਾ ਕਰਨ ਲਈ ਇੱਕ ਪਬਲਿਕ IP ਐਡਰੈੱਸ ਨਕਸ਼ੇ.';
+$lang['nat_firewall_app_name'] = '1-ਨੂੰ-1 NAT ਫਾਇਰਵਾਲ';
+$lang['nat_firewall_lan_ip'] = 'LAN ਦਾ IP';
+$lang['nat_firewall_no_external_nic'] = '1-ਨੂੰ-1 NAT ਘੱਟੋ-ਘੱਟ ਇੱਕ ਬਾਹਰੀ ਸੰਰਚਿਤ ਨੈੱਟਵਰਕ ਇੰਟਰਫੇਸ ਦੀ ਲੋੜ ਹੈ.';
+$lang['nat_firewall_port_or_port_range'] = 'ਪੋਰ੍ਟ ਜ ਪੋਰ੍ਟ ਸੀਮਾ';
+$lang['nat_firewall_private_ip'] = 'ਪ੍ਰਾਈਵੇਟ IP ਨੂੰ';
+$lang['nat_firewall_public_ip'] = 'ਪਬਲਿਕ IP';
+$lang['nat_firewall_rules'] = '1-ਨੂੰ-1 NAT ਨਿਯਮ';
+$lang['nat_firewall_tooltip'] = 'ਇਹ ਫੀਚਰ ਆਪਣੇ ਇੰਟਰਨੈੱਟ ਸਰਵਿਸ ਪਰੋਵਾਈਡਰ (ISP) ਤੱਕ ਵਾਧੂ ਪਬਲਿਕ IP ਐਡਰੈੱਸ ਦੀ ਲੋੜ ਹੈ.';
+$lang['nat_firewall_wan_ip'] = 'WAN IP ਨੂੰ';
