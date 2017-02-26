4d6d27d7cb9b67bd18111417fe1e650a10dd9071
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:04:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Pievieno 1-to-1 NAT Rule';
+$lang['nat_firewall_app_description'] = '1-to-1 NAT Firewall app kartes publisko IP adresi uz privāto IP adresi, kas ļauj piekļūt sistēmām aiz ugunsmūra, izmantojot publisko IP adresi.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT ugunsmūris';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT ir nepieciešama vismaz viena no ārpuses ir konfigurēta tīkla interfeisu.';
+$lang['nat_firewall_port_or_port_range'] = 'Port vai Port Range';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'publiskā IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT noteikumi';
+$lang['nat_firewall_tooltip'] = 'Šī funkcija ir nepieciešama papildu publisko IP adreses no jūsu interneta pakalpojumu sniedzējs (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
