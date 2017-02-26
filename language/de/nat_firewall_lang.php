e577dde0faa9c95d73aab63ce239c4a5bdf780ad
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:00:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1-zu-1-NAT-Regel';
+$lang['nat_firewall_app_description'] = 'Die 1-zu-1-NAT-Firewall App bildet eine öffentliche IP-Adresse zu einer privaten IP-Adresse für den Zugang zu Systemen hinter der Firewall über eine öffentliche IP-Adresse.';
+$lang['nat_firewall_app_name'] = '1-zu-1-NAT-Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN-IP';
+$lang['nat_firewall_no_external_nic'] = '1-zu-1-NAT erfordert mindestens eine extern konfigurierten Netzwerkschnittstelle.';
+$lang['nat_firewall_port_or_port_range'] = 'Port oder Portbereich';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Öffentliche IP';
+$lang['nat_firewall_rules'] = '1-zu-1-NAT-Regeln';
+$lang['nat_firewall_tooltip'] = 'Diese Funktion erfordert zusätzliche öffentliche IP-Adressen von Ihrem Internet Service Provider (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN-IP';
