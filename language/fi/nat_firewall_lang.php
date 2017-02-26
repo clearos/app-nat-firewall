38f866f8fcc68e309437c04ba14acaa27889976b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:00:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Lisää 1-to-1 NAT sääntö';
+$lang['nat_firewall_app_description'] = '1-to-1 NAT palomuuri sovellus kartoittaa julkisen IP-osoitteen yksityiseen IP-osoitteen päästämällä järjestelmiin taakse palomuurin kautta julkinen IP-osoite.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT palomuuri';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT vaatii vähintään yhden ulkoisesti määritetty verkkoliitäntä.';
+$lang['nat_firewall_port_or_port_range'] = 'Port tai porttiväli';
+$lang['nat_firewall_private_ip'] = 'yksityinen IP';
+$lang['nat_firewall_public_ip'] = 'julkinen IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT säännöt';
+$lang['nat_firewall_tooltip'] = 'Tämä ominaisuus vaatii lisää julkisia IP-osoitteita Internet-palveluntarjoaja (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
