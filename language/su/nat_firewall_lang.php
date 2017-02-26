eeabd0c3d5d30ac923e202f9d3f0711293dbc823
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:07:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Tambah Aturan 1-to-1 Nat';
+$lang['nat_firewall_app_description'] = '1-to-1 Nat firewall app peta alamat IP umum ka alamat IP swasta ngamungkinkeun aksés ka sistem balik firewall nu via alamat IP umum.';
+$lang['nat_firewall_app_name'] = '1-to-1 Nat firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 Nat merlukeun sahenteuna hiji interface jaringan ngonpigurasi externally.';
+$lang['nat_firewall_port_or_port_range'] = 'Port atawa Port Range';
+$lang['nat_firewall_private_ip'] = 'wasta IP';
+$lang['nat_firewall_public_ip'] = 'IP public';
+$lang['nat_firewall_rules'] = '1-to-1 Aturan Nat';
+$lang['nat_firewall_tooltip'] = 'Fitur ieu merlukeun tambahan Alamat IP umum ti Anjeun Provider Service Internet (ISP).';
+$lang['nat_firewall_wan_ip'] = 'IP Wan';
