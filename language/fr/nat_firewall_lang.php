6b43f4130f3135fcc1de984b1a40fdde827e5ca2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:00:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Ajouter une règle 1-to-1 NAT';
+$lang['nat_firewall_app_description'] = 'Le pare-feu NAT app 1-to-1 mappe une adresse IP publique à une adresse IP privée permettant l&#39;accès aux systèmes derrière le pare-feu via une adresse IP publique.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'IP LAN';
+$lang['nat_firewall_no_external_nic'] = '1 à 1 NAT nécessite au moins une interface de réseau externe configurée.';
+$lang['nat_firewall_port_or_port_range'] = 'Port ou Port Range';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'IP publique';
+$lang['nat_firewall_rules'] = '1-to-1 Règles NAT';
+$lang['nat_firewall_tooltip'] = 'Cette fonction nécessite d&#39;autres adresses IP publique de votre fournisseur d&#39;accès Internet (FAI).';
+$lang['nat_firewall_wan_ip'] = 'IP WAN';
