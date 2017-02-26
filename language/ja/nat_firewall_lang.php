458b8940ab4d4827f3a40ed5b4838e2b8484f865
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:02:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1対1 NATルールを追加します。';
+$lang['nat_firewall_app_description'] = '1対1のNATファイアウォールのアプリは、パブリックIPアドレスを経由して、ファイアウォールの背後のシステムへのアクセスを可能にするプライベートIPアドレスにパブリックIPアドレスをマッピングします。';
+$lang['nat_firewall_app_name'] = '1対1のNATファイアウォール';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1対1のNATは、少なくとも一つの外部で構成されたネットワークインターフェースが必要です。';
+$lang['nat_firewall_port_or_port_range'] = 'ポートまたはポート範囲';
+$lang['nat_firewall_private_ip'] = 'プライベートIP';
+$lang['nat_firewall_public_ip'] = 'パブリックIP';
+$lang['nat_firewall_rules'] = '1対1 NATのルール';
+$lang['nat_firewall_tooltip'] = 'この機能は、インターネットサービスプロバイダ（ISP）からの追加のパブリックIPアドレスが必要です。';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
