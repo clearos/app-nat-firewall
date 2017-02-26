bfcabd9d01bc57c4c0ef2fe961aa910b0068fa07
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:01:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Ajoute Règleman 1-a-1 NAT';
+$lang['nat_firewall_app_description'] = '1-a-1 NAT Firewall App a kat yon adrès IP piblik nan yon adrès IP prive ki pèmèt aksè nan sistèm dèyè pare-feu lan atravè yon adrès IP piblik.';
+$lang['nat_firewall_app_name'] = '1-a-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-a-1 NAT mande pou omwen yon deyò configuré koòdone rezo-a.';
+$lang['nat_firewall_port_or_port_range'] = 'Port oswa Port Range';
+$lang['nat_firewall_private_ip'] = 'Prive IP';
+$lang['nat_firewall_public_ip'] = 'Piblik IP';
+$lang['nat_firewall_rules'] = '1-a-1 Règ NAT';
+$lang['nat_firewall_tooltip'] = 'Karakteristik sa a mande pou plis adrès IP piblik nan Founisè Sèvis Entènèt ou (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
