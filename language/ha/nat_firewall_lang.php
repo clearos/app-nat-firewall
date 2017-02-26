96ebd96dfccefa3fd778e0261ad5e9fa0aaed553
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:01:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Add 1-to-1 NAT Rule';
+$lang['nat_firewall_app_description'] = 'The 1-to-1 NAT Firewall app maps jama&#39;a IP address zuwa masu zaman kansu IP address kyale access to tsarin baya da Firewall via jama&#39;a IP address.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT bukatar a kalla daya externally kaga cibiyar ke dubawa.';
+$lang['nat_firewall_port_or_port_range'] = 'Port ko Port Range';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Public IP';
+$lang['nat_firewall_rules'] = '1-to-1 NAT Dokokin';
+$lang['nat_firewall_tooltip'] = 'Wannan alama na bukatar ƙarin jama&#39;a IP adiresoshin daga Sabis na Intanet (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
