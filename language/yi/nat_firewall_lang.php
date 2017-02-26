2faaa9d70a6d1c7dc1542333358a24e6dda8733f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:09:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'לייג צו 1-צו -1 נאַט הערשן';
+$lang['nat_firewall_app_description'] = 'די 1-צו -1 נאַט Firewall אַפּ מאַפּס אַ ציבור יפּ אַדרעס צו אַ פּריוואַט IP אַדרעס אַלאַוינג צוטריט צו סיסטעמען הינטער די Firewall דורך אַ ציבור יפּ אַדרעס.';
+$lang['nat_firewall_app_name'] = '1-צו -1 נאַט Firewall';
+$lang['nat_firewall_lan_ip'] = 'לאַן יפּ';
+$lang['nat_firewall_no_external_nic'] = '1-צו -1 נאַט ריקווייערז בייַ מינדסטער איינער ויסווייניק קאָנפיגורעד נעץ צובינד.';
+$lang['nat_firewall_port_or_port_range'] = 'פּאָרט אָדער פּאָרט ראַנגע';
+$lang['nat_firewall_private_ip'] = 'פּריוואַט יפּ';
+$lang['nat_firewall_public_ip'] = 'ציבור יפּ';
+$lang['nat_firewall_rules'] = '1-צו -1 נאַט כּללים';
+$lang['nat_firewall_tooltip'] = 'דעם שטריך ריקווייערז נאָך עפנטלעך IP ווענדט זיך פֿון אייער אינטערנעט סערוויס פּראַוויידער (יספּ).';
+$lang['nat_firewall_wan_ip'] = 'וואַן יפּ';
