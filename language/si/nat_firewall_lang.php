33444bf0e1b801a19cdebb56db583b7e3ee1e1fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:06:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = '1 දක්වා-1 ජාල ලිපින පරිවර්තකයේ පාලනය එකතු කරන්න';
+$lang['nat_firewall_app_description'] = '1-කිරීමට 1 ජාල ලිපින පරිවර්තකයේ ගිනි පවුර යෙදුම ප්රසිද්ධ IP ලිපිනය හරහා ගිනි පිටුපස පද්ධති ලබා ගැනීමට ඉඩ පෞද්ගලික IP ලිපිනය කිරීමට පොදු IP ලිපින සිතියම්.';
+$lang['nat_firewall_app_name'] = '1 දක්වා-1 ජාල ලිපින පරිවර්තකයේ ගිනි පවුර';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1 දක්වා-1 ජාල ලිපින පරිවර්තකයේ බාහිර වින්යාසගත අවම වශයෙන් එක් ජාල අතුරු මුහුණත අවශ්ය වේ.';
+$lang['nat_firewall_port_or_port_range'] = 'වරාය හෝ වරාය රංගේ';
+$lang['nat_firewall_private_ip'] = 'පුද්ගලික IP';
+$lang['nat_firewall_public_ip'] = 'පොදු IP';
+$lang['nat_firewall_rules'] = '1 දක්වා-1 ජාල ලිපින පරිවර්තකයේ රීති';
+$lang['nat_firewall_tooltip'] = 'මෙම ලක්ෂණය ඔබගේ අන්තර්ජාල සේවා සපයන්නා (ISP) අතිරේක පොදු IP ලිපින අවශ්ය වේ.';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
