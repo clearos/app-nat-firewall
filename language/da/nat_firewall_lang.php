d135174173127a338ee75bb879b534ae8401d2e8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:00:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Tilsæt 1-til-1 NAT Rule';
+$lang['nat_firewall_app_description'] = 'Den 1-til-1 NAT firewall app kortlægger en offentlig IP-adresse til en privat IP-adresse giver adgang til systemerne bag firewallen via en offentlig IP-adresse.';
+$lang['nat_firewall_app_name'] = '1-til-1 NAT firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-til-1 NAT kræver mindst en eksternt konfigureret netværksinterface.';
+$lang['nat_firewall_port_or_port_range'] = 'Port eller Port Range';
+$lang['nat_firewall_private_ip'] = 'Private IP';
+$lang['nat_firewall_public_ip'] = 'Offentlig IP';
+$lang['nat_firewall_rules'] = '1-til-1 NAT Regler';
+$lang['nat_firewall_tooltip'] = 'Denne funktion kræver yderligere offentlige IP-adresser fra din Internet Service Provider (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
