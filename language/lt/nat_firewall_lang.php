45431b8a6aa4bc6f57d422db25424dea3d665b2c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:03:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Pridėti 1 iki 1 NAT taisyklė';
+$lang['nat_firewall_app_description'] = '1-su-1 NAT ugniasienė programa žemėlapiai viešą IP adresą privačiai IP adresą, leidžianti prieigą prie sistemų už užkardos viešuoju IP adresą.';
+$lang['nat_firewall_app_name'] = '1 iki 1 NAT ugniasienė';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-į-1 NAT reikalauja ne mažiau kaip vieną iš išorės, sukonfigūruotą tinklo sąsają.';
+$lang['nat_firewall_port_or_port_range'] = 'Uosto ar uosto klasės';
+$lang['nat_firewall_private_ip'] = 'Privati ​​IP';
+$lang['nat_firewall_public_ip'] = 'Viešas IP adresas';
+$lang['nat_firewall_rules'] = '1 iki 1 NAT Taisyklės';
+$lang['nat_firewall_tooltip'] = 'Ši funkcija reikalauja papildomų viešojo IP adresus iš savo interneto paslaugų teikėjo (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
