bfb6a3bfe8497313753eda780e1aaf706407b6ed
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 17:09:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/nat_firewall_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['nat_firewall_add_nat_rule'] = 'Thêm quy tắc 1-to-1 NAT';
+$lang['nat_firewall_app_description'] = 'NAT Firewall ứng dụng 1-to-1 ánh xạ một địa chỉ IP công cộng đến một địa chỉ IP riêng cho phép truy cập vào hệ thống đằng sau các bức tường lửa thông qua một địa chỉ IP công cộng.';
+$lang['nat_firewall_app_name'] = '1-to-1 NAT Firewall';
+$lang['nat_firewall_lan_ip'] = 'LAN IP';
+$lang['nat_firewall_no_external_nic'] = '1-to-1 NAT đòi hỏi ít nhất một giao diện mạng bên ngoài được cấu hình.';
+$lang['nat_firewall_port_or_port_range'] = 'Cảng hoặc Port Range';
+$lang['nat_firewall_private_ip'] = 'IP riêng';
+$lang['nat_firewall_public_ip'] = 'IP công cộng';
+$lang['nat_firewall_rules'] = '1-to-1 quy NAT';
+$lang['nat_firewall_tooltip'] = 'Tính năng này đòi hỏi phải có thêm các địa chỉ IP công cộng từ nhà cung cấp dịch vụ Internet (ISP).';
+$lang['nat_firewall_wan_ip'] = 'WAN IP';
